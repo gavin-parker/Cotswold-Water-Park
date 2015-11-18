fa10f84bd958994e99a93449273692bb8612ac9e
Gavin Parker <gp14958@my.bristol.ac.uk>
2015-11-18 16:36:27 +0000

    split map js into seperate controller and removed button

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,4 +17,16 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+});
+
+app.controller('MapCtrl', function(){
+  var init = function(){
+    var map = new L.Map('map');
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	  var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});
+    map.setView(new L.LatLng(51.644224, -1.965172), 13);
+    map.addLayer(osm);
+  };
+  init();
 });
