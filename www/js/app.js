8034e85c60d09fef0d620d044852be67deff6c81
Gavin Parker <gp14958@my.bristol.ac.uk>
2015-12-09 16:15:37 +0000

    added geolocation through device gps

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,12 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic'])
 
 app.config(['$ionicConfigProvider', function($ionicConfigProvider) {
 
-    $ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.tabs.position('bottom');
 
 }]);
 
-
+//Setup functions
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -24,30 +19,47 @@ app.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
 
+
+    // onError Callback receives a PositionError object
+    //
+
+
+
   });
-
-
-
 });
-
+//controller manipulating map
 app.controller('MapCtrl', function(){
+  var map = new L.Map('map');
+  var x = 51.659611;
+  var y = -1.913410;
+
+  var getLoc = function(position) {
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    map.setView(new L.LatLng(x, y), 13);
+  };
+
+  function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+    'message: ' + error.message + '\n');
+  };
+
   var init = function(){
-    var map = new L.Map('map');
 
     var markerIcon = L.Icon.extend({
-    options: {
+      options: {
         iconSize:     [45, 45],
         iconAnchor:   [10, 40],
         popupAnchor:  [10, -20]
-    }
-});
-    //var lc  =  L.control.locate().addTo(map);  //Will have to disable for the time being, geolocation should only be an option if person is within map bounds.
-    //lc.start();
-    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	  var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});
-    map.setView(new L.LatLng(51.635434, -1.935172), 13);
+      }
+    });
+    navigator.geolocation.getCurrentPosition(getLoc, onError);
+    var osmUrl='http://{s}.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});
+    map.setView(new L.LatLng(x, y), 13);
     var bounds = map.getBounds();
+
     map.setMaxBounds(bounds);
     map.addLayer(osm);
     var blueIcon = new markerIcon({iconUrl: '/img/marker.png'});
@@ -55,11 +67,7 @@ app.controller('MapCtrl', function(){
   };
   init();
 });
-
-//function in case want to add information
-app.controller('InfoCtrl', function(){
-});
-
+//function to control activities tab
 app.controller('ActivitiesCtrl', function($scope, parkDataService){
   $scope.activities = parkDataService.activities();
   $scope.num = parkDataService.activities.length;
@@ -76,10 +84,10 @@ app.controller('ActivitiesCtrl', function($scope, parkDataService){
   };
 });
 
-
+//controls events tab
 app.controller('EventsCtrl', function($scope, eventService){
   function initialize(){
-      eventService.Feed().then(function(result){
+    eventService.Feed().then(function(result){
       $scope.events = result.feed.entries;
       console.log($scope.events);
     });
@@ -99,9 +107,10 @@ app.controller('EventsCtrl', function($scope, eventService){
   };
 
 });
+//controls news tab
 app.controller('NewsCtrl', function($scope, newsService){
   function initialize(){
-      newsService.Feed().then(function(result){
+    newsService.Feed().then(function(result){
       $scope.news = result.feed.entries;
       console.log($scope.news);
     });
@@ -122,42 +131,43 @@ app.controller('NewsCtrl', function($scope, newsService){
 
 });
 
+//service to get hardcoded activity data
 app.factory('parkDataService', function(){
   var activities =
   [
-  {
-    name: "Angling",
-    info: "Fishing takes place on more than 70 lakes in the Cotswold Water Park, with the clear waters and peaceful locations making this an ideal spot for both coarse and fly fishing. Day tickets and club membership facilities are available from fisheries and tackle shops in the area and nearby.",
-    data: [
-      {
-        name: "Lake Pochard",
-        number: "01793 751 513",
-        email: "contact@lakepochard.co.uk",
-        url: "www.lakepochard.co.uk",
-        info: "Fully equipped Scandinavian-style Lodges, all with verandas and views across the 10-acre lake. Available for short or week breaks. Top quality carp fishing and large, airy Waterside Café on site.",
-      },
-      {
-        name: "Tackle Den",
-        number: "01285 862 716",
-        email: "thetackleden@hotmail.co.uk",
-        url: "www.facebook/com/tackleden",
-        info: "Now bigger and better! Carp and coarse fishing specialist. The no. 1 tackle shop in the Cotswold Water Park, selling numerous brands of angling equipment and bait"
-      }
-    ]
-  },
-  {
-    name: "Boat Trips",
-    info: "",
-    data: [
-      {
-        name: "Cotswold Canals Trust Boat Trips",
-        number: "07787 485 294",
-        email: "mail@cotswoldcanals.com",
-        url: "www.lechladetripboat.com",
-        info: "Enjoy the gentle meadering River Thames aboard the beautiful launch 'Inglesham' departing from Riverside Park. GL7 3AG . All proceeds invested in the restoration of the Cotswold canals."
-      }
-    ]
-  },
+    {
+      name: "Angling",
+      info: "Fishing takes place on more than 70 lakes in the Cotswold Water Park, with the clear waters and peaceful locations making this an ideal spot for both coarse and fly fishing. Day tickets and club membership facilities are available from fisheries and tackle shops in the area and nearby.",
+      data: [
+        {
+          name: "Lake Pochard",
+          number: "01793 751 513",
+          email: "contact@lakepochard.co.uk",
+          url: "www.lakepochard.co.uk",
+          info: "Fully equipped Scandinavian-style Lodges, all with verandas and views across the 10-acre lake. Available for short or week breaks. Top quality carp fishing and large, airy Waterside Café on site.",
+        },
+        {
+          name: "Tackle Den",
+          number: "01285 862 716",
+          email: "thetackleden@hotmail.co.uk",
+          url: "www.facebook/com/tackleden",
+          info: "Now bigger and better! Carp and coarse fishing specialist. The no. 1 tackle shop in the Cotswold Water Park, selling numerous brands of angling equipment and bait"
+        }
+      ]
+    },
+    {
+      name: "Boat Trips",
+      info: "",
+      data: [
+        {
+          name: "Cotswold Canals Trust Boat Trips",
+          number: "07787 485 294",
+          email: "mail@cotswoldcanals.com",
+          url: "www.lechladetripboat.com",
+          info: "Enjoy the gentle meadering River Thames aboard the beautiful launch 'Inglesham' departing from Riverside Park. GL7 3AG . All proceeds invested in the restoration of the Cotswold canals."
+        }
+      ]
+    },
   ];
 
   return {
@@ -167,6 +177,7 @@ app.factory('parkDataService', function(){
   }
 });
 
+//gets live event data via RSS feed (superfeedr API)
 app.factory('eventService', function($q){
   return{
     Feed : function(){
@@ -190,6 +201,7 @@ app.factory('eventService', function($q){
 
 });
 
+//gets live news data via RSS feed (superfeedr API)
 app.factory('newsService', function($q){
   return{
     Feed : function(){
@@ -206,7 +218,6 @@ app.factory('newsService', function($q){
           console.log("feed error");
         };
       });
-
       return defer.promise;
     }
   }
