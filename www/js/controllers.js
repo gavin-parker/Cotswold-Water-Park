1031a52b128e1d6437c19c97907cbcbad72ac4ff
Gavin Parker <gp14958@my.bristol.ac.uk>
2015-12-09 16:32:01 +0000

    code organisation and modularisation

---
+++ www/js/controllers.js
@@ -0,0 +1,104 @@
+
+//controller manipulating map
+app.controller('MapCtrl', function(){
+  var map = new L.Map('map');
+  var x = 51.659611;
+  var y = -1.913410;
+
+  var getLoc = function(position) {
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    map.setView(new L.LatLng(x, y), 13);
+  };
+
+  function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+    'message: ' + error.message + '\n');
+  };
+
+  var init = function(){
+
+    var markerIcon = L.Icon.extend({
+      options: {
+        iconSize:     [45, 45],
+        iconAnchor:   [10, 40],
+        popupAnchor:  [10, -20]
+      }
+    });
+    navigator.geolocation.getCurrentPosition(getLoc, onError);
+    var osmUrl='http://{s}.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});
+    map.setView(new L.LatLng(x, y), 13);
+    var bounds = map.getBounds();
+
+    map.setMaxBounds(bounds);
+    map.addLayer(osm);
+    var blueIcon = new markerIcon({iconUrl: 'img/marker.png'});
+    L.marker([51.659611, -1.913410], {icon: blueIcon}).addTo(map).bindPopup("Lake Pochard Lodges");
+  };
+  init();
+});
+//function to control activities tab
+app.controller('ActivitiesCtrl', function($scope, parkDataService){
+  $scope.activities = parkDataService.activities();
+  $scope.num = parkDataService.activities.length;
+
+  $scope.toggleGroup = function(activity) {
+    if ($scope.isGroupShown(activity)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = activity;
+    }
+  };
+  $scope.isGroupShown = function(activity) {
+    return $scope.shownGroup === activity;
+  };
+});
+
+//controls events tab
+app.controller('EventsCtrl', function($scope, eventService){
+  function initialize(){
+    eventService.Feed().then(function(result){
+      $scope.events = result.feed.entries;
+      console.log($scope.events);
+    });
+  }
+  superfeedr.auth('gp14958','df172f3202b13c654d4777881720c9cd');
+  superfeedr.setOnLoadCallback(initialize);
+
+  $scope.toggleGroup = function(activity) {
+    if ($scope.isGroupShown(activity)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = activity;
+    }
+  };
+  $scope.isGroupShown = function(activity) {
+    return $scope.shownGroup === activity;
+  };
+
+});
+//controls news tab
+app.controller('NewsCtrl', function($scope, newsService){
+  function initialize(){
+    newsService.Feed().then(function(result){
+      $scope.news = result.feed.entries;
+      console.log($scope.news);
+    });
+  }
+  superfeedr.auth('gp14958','df172f3202b13c654d4777881720c9cd');
+  superfeedr.setOnLoadCallback(initialize);
+
+  $scope.toggleGroup = function(activity) {
+    if ($scope.isGroupShown(activity)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = activity;
+    }
+  };
+  $scope.isGroupShown = function(activity) {
+    return $scope.shownGroup === activity;
+  };
+
+});
