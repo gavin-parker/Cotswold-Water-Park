42ff92d30e7f02fc999a2040239fabe453747d45
Gavin Parker <gp14958@my.bristol.ac.uk>
2016-03-02 12:33:14 +0000

    added lake locs file

--- www/js/factories.js
+++ www/js/factories.js
@@ -99,18 +99,18 @@ app.factory('tweetService', function($q){
 });
 
 app.factory('Camera', ['$q', function($q) {
- 
+
   return {
     getPicture: function(options) {
       var q = $q.defer();
-      
+
       navigator.camera.getPicture(function(result) {
         // Do any magic you need
         q.resolve(result);
       }, function(err) {
         q.reject(err);
       }, options);
-      
+
       return q.promise;
     }
   }
