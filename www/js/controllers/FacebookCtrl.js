f05015db78f605326ed5567bd54cec816a70aae1
TuanaCelik <tc13007@my.bristol.ac.uk>
2016-03-09 10:39:45 +0000

    Tried something for FB view

--- www/js/controllers/FacebookCtrl.js
+++ www/js/controllers/FacebookCtrl.js
@@ -2,6 +2,6 @@ app.controller('FacebookCtrl', function($scope){
 	$scope.openCordovaWebView = function()
 	{
 	 // Open cordova webview if the url is in the whitelist otherwise opens in app browser
-	 window.open('https://www.facebook.com/CotswoldWaterParkTrust','_self'); 
+	 window.open('https://www.facebook.com/CotswoldWaterParkTrust','_self', hardwareback = 'yes', toolbar = 'yes'); 
 	};
 });
